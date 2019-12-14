a9f6fa5279e5cf584a3648b8ddcb18260c66a108
Jaroslav Bušek <nemotora@users.noreply.github.com>
2019-12-14 12:40:12 +0100

    Add files via upload

--- sketch_dec14c.ino
+++ sketch_dec14c.ino
@@ -1,9 +1,11 @@
 #include <Adafruit_NeoPixel.h>
+#include <ButtonDebounce.h>
 
 #define PIN_LED       6
 #define POCET_LED     8
 
 Adafruit_NeoPixel svetylka(POCET_LED, PIN_LED, NEO_RGB + NEO_KHZ800);
+ButtonDebounce tlacitko1(5, 250);
 
 byte cervena = 0;
 byte rezim = 0;
@@ -13,6 +15,7 @@ void setup() {
   svetylka.setBrightness(63);
   svetylka.fill(svetylka.Color(0, 0, 255));
   svetylka.show();
+  tlacitko1.setCallback(tlacitko1Stisknuto);
 }
 
 void loop() {
@@ -26,5 +29,11 @@ void loop() {
     svetylka.fill(svetylka.Color(random(255), random(255), random(255)));
     svetylka.show();
     delay(100);
+  }
+}
+
+void tlacitko1Stisknuto(int stav) {
+  if (!stav) {
+    rezim++;
   }
 }
