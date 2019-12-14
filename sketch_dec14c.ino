421ef10469125a860d9b89150946853d0c464820
Jaroslav Bušek <nemotora@users.noreply.github.com>
2019-12-14 12:35:01 +0100

    Add files via upload

---
+++ sketch_dec14c.ino
@@ -0,0 +1,27 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN_LED       6
+#define POCET_LED     8
+
+Adafruit_NeoPixel svetylka(POCET_LED, PIN_LED, NEO_RGB + NEO_KHZ800);
+
+byte cervena = 0;
+
+void setup() {
+  svetylka.begin();
+  svetylka.setBrightness(63);
+  svetylka.fill(svetylka.Color(0, 0, 255));
+  svetylka.show();
+}
+
+void loop() {
+  /*
+  svetylka.fill(svetylka.Color(cervena, 52, 189));
+  svetylka.show();
+  cervena = cervena + 1;
+  delay(20);
+  */
+  svetylka.fill(svetylka.Color(random(255), random(255), random(255)));
+  svetylka.show();
+  delay(100);
+}
